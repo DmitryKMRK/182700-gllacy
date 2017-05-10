8476675e84f9933ef5f478e72019cec6b5529611
DmitryKMRK <dmitry.kmrk@gmail.com>
2017-05-11 00:01:01 +0300

    Доработка

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,17 @@
 var link = document.querySelector(".contacts-button");
 
 var popup = document.querySelector(".contacts-form");
+var overlay = document.querySelector(".contacts-form-overlay");
 var close = popup.querySelector(".contacts-close-button");
 
 link.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.add("contacts-form-show");
+  overlay.classList.add("contacts-form-overlay-show");
 });
 
 close.addEventListener("click", function(event) {
   event.preventDefault();
   popup.classList.remove("contacts-form-show");
+  overlay.classList.remove("contacts-form-overlay-show");
 });
